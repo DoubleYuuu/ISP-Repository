6390b1b036437640b8bd22af6dc715b36438fe03
parniaYzp <parnia.yzp@gmail.com>
2024-06-07 12:58:35 -0400

    Deleted the test lines from player class

--- Player/player.java
+++ Player/player.java
@@ -3,9 +3,7 @@ package Player;
 public class player {
 
     public static void main(String[] args) { 
-        //
-    //asjkdhkajshdjkhaskjdhkjhas
-    	System.out.println("hello world");
+        
     }
 
 }
