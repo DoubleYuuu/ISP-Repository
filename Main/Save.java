b51f699de231898ed025d1a1f1e94997b32d4979
1mSunmin <363292871@gotvdsb.ca>
2024-06-07 09:17:13 -0400

    main

--- Main/Save.java
+++ Main/Save.java
@@ -18,14 +18,34 @@ public class Save {
 	
 	public static void writedown() throws IOException{
 		RandomAccessFile raf = new RandomAccessFile(filename,"rw");
-		gameinfo.writeRec(raf);
+		 raf.seek (recordLength); // move pointer to position in file
+		int padLen = 0; // the number of blanks that needed to be added to maintain a length of 10
+		int IDLen = gameinfo.getID().length(); // determine if there are more than 10 characters
+		if (IDLen > 10)
+			IDLen = 10;
+        else
+            padLen = 10 - IDLen;
+		
+		// other fields from game should be processed to be written down
+		 /*
+         * writeLong()
+         * writeDouble()
+         * writeInt()
+         * writeChar() string-->char
+         */
+		
 	    raf.close();
 	}
 	
-	public static int readin() throws IOException{
+	public static void readin() throws IOException{
 		RandomAccessFile raf = new RandomAccessFile(filename,"rw");
-		gameinfo = new Game();
-		gameinfo.readRec(raf);
+		String newID = "";
+		for (int i=0; i<10; i++)
+			newID = newID + raf.readChar();
+		gameinfo = new Game(newID);
+		
+		// other fields from game should be processed to be written down
+		
 		raf.close();
 	}
 }
