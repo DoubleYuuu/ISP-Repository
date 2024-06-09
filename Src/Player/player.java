220b1cfe6bef4bafa6ef36511163a6996fe61bf7
Devadath Pillai <159266392+Dev3630@users.noreply.github.com>
2024-06-09 00:32:08 -0400

    buttered stuff up, added placeholders

--- Src/Player/player.java
+++ Src/Player/player.java
@@ -1,71 +1,90 @@
 package Player;
+
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import util.LoadSave;
 
+public class player {
 
 
 
-private BufferedImage statusBarImg;
+    private BufferedImage statusBarImg;
 
-private int statusBarWidth = (int) (815 * Game.SCALE);
-private int statusBarHeight = (int) (230 * Game.SCALE);
-private int statusBarX = (int) (10 * Game.SCALE);
-private int statusBarY = (int) (10 * Game.SCALE);
+    private int statusBarWidth = (int) (815 * Game.SCALE);
+    private int statusBarHeight = (int) (230 * Game.SCALE);
+    private int statusBarX = (int) (10 * Game.SCALE);
+    private int statusBarY = (int) (10 * Game.SCALE);
 
-private int statusBarWidth = (int) (553 * Game.SCALE);
-private int statusBarHeight = (int) (67 * Game.SCALE);
-private int statusBarX = (int) (10 * Game.SCALE);
-private int statusBarY = (int) (10 * Game.SCALE);
+    private int maxHealth = 100;
+    private int currentHealth = maxHealth;
 
-private int maxHealth = 100;
-private int currentHealth = maxHealth;
-private int healthWidth = healthBarWidth;
+    private int healthBarWidth = (int) (553 * Game.SCALE);
+    private int healthBarHeight = (int) (67 * Game.SCALE);
+    private int healthBarXStart = (int) (10 * Game.SCALE);
+    private int healthBarYStart = (int) (10 * Game.SCALE);
 
-public void update(){
+    private int healthWidth = healthBarWidth;
 
-    updateHealthBar();
+    public void update(){
 
-    updatePOS();
-    updateAnimationTick();
-    setAnimation();
+        updateHealthBar();
+        updatePOS();
+        updateAnimationTick();
+        setAnimation();
+
+    }
+
+    private void updateHealthBar(){
+
+        healthWidth = (int)((currentHealth / (float)maxHealth) * healthBarWidth);
+
+
+    }
+
+    public void render(Graphics g) {
+
+        drawUI(g);
+
+    }
+
+    private void drawUI(Graphics g) {
+
+        g.drawImage(statusBarImg, statusBarX, statusBarY, statusBarWidth, statusBarHeight, null);
+        g.setColor(Color.red);
+        g.fillRect(healthBarXStart + statusBarX, healthBarYStart + statusBarY, healthWidth, healthBarHeight);
+
+    }
+
+    public void changeHealth(int value) {
+
+        currentHealth += value;
+
+        if(currentHealth <= 0) {
+            currentHealth = 0;
+        } else if (currentHealth >= maxHealth) {
+            currentHealth = maxHealth;
+        }
+    }
+
+    private void loadAnimations() {
+
+        statusBarImg = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
+
+    }
+
+    //these are placeholders for now, until Imad does player:
+
+    private void updatePOS() { //POS being like position
+
+    }
+
+    private void updateAnimationTick() { //updates aniamtion tick
+
+    }
+
+    private void setAnimation() { //setting the anims
+
+    }
 
 }
-
-private void updateHealthBar(){
-
-    healthWidth = (int)((currentHealth / (float)maxHealth) * healthBarWidth);
-
-
-}
-
-public void render() {
-
-    drawUI(g);
-
-}
-
-private void drawUI(Graphics g) {
-    g.drawImage(statusBarImg, statusBarX, statusBarY, statusBarWidth, statusBarHeight, null);
-    g.setColor(Color.red);
-    g.fillRect(healthBarXStart + statusBarX, healthBarYStart + statusBarY, healthWidth, healthBarHeight);
-
-    drawUI(g);
-
-}
-
-public void changeHealth(int value) {
-
-    currentHealth += value;
-
-    if(currentHealth <= 0) {
-        currentHealth = 0;
-    } else if (currentHealth >= maxHealth)
-        currentHealth = maxHealth;
-}
-
-private void loadAnimations() {
-
-     statusBarImg = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
-
-}
-
