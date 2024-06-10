890a1c2e415b88f5b06b8f44ab7de3f2e700ce37
Devadath Pillai <159266392+Dev3630@users.noreply.github.com>
2024-06-10 08:48:36 -0400

    oh my god please work please work please work

--- Src/Weapons/Arrows.java
+++ Src/Weapons/Arrows.java
@@ -1,12 +1,236 @@
 package Weapons;
+
+import GameStates.Playing;
+import Entities.Entity;
+import static Utilities.Constants.GAME_WIDTH;
+import static Utilities.Constants.GAME_HEIGHT;
+import static Utilities.Atlas.ARROW_ATLAS;
+import static Utilities.Atlas.getSpriteAtlas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.util.ConcurrentModificationException;
 
-public class Arrows{
+public class Arrows extends Entity implements MouseListener {
+
+    private BufferedImage img, arrowNormal, arrowSpecial;
+    private double x, y, directionX, directionY;
+    private Weapon1 weapon;
+    private Playing playing;
+    private int[][] lvlData;
+    private double time;
+    private boolean shoot = false;
+    private double theta = 0;
+    private int damage;
+
+    private static final double SPEED1 = 10.0;
+    private static final double SPEED2 = 7.0;
+    private static final double SPEED3 = 1.0;
+
+    public Arrows(Weapon1 weapon, Playing playing, double startX, double startY, double targetX, double targetY, int xOffset, int[][] lvlData, double time) {
+
+        super((float) startX - 5, (float) startY - 5, 22, 22);
+        this.weapon = weapon;
+        this.playing = playing;
+        this.x = startX;
+        this.y = startY;
+        this.lvlData = lvlData;
+        this.time = time;
+
+        setDirection(targetX, targetY, xOffset);
+        setTime();
+        getImage();
+        initialize();
+
+        if (!playing.getPlayer().isDead()) {
+
+            playing.getSoundLibrary().playSound("Shoot");
+
+        }
+
+        
+        if (Playing.gunIndex == 1) {
+
+            this.damage = 10; 
+
+        } else if (Playing.gunIndex == 2) {
+
+            this.damage = 20; 
+
+        } else if (Playing.gunIndex == 3) {
+
+            this.damage = 30; 
+
+        }
+
+    }
 
     
+    public void setDirection(double targetX, double targetY, int xOffset) {
+
+        double angle = Math.atan2(targetY - y, targetX - x + xOffset);
+        this.directionX = Math.cos(angle);
+        this.directionY = Math.sin(angle);
+
+    }
+
+    
+    public void move() {
+
+        double speed = (Playing.bowIndex == 1) ? SPEED1 : (Playing.bowIndex == 2) ? SPEED2 : SPEED3;
+
+        if (canMove((float) (hitbox.x + speed * directionX), (float) (hitbox.y + speed * directionY), hitbox.width, hitbox.height, lvlData) && !stuckBehindDoor) {
+
+            x += speed * directionX;
+            y += speed * directionY;
+            hitbox.x += speed * directionX;
+            hitbox.y += speed * directionY;
+
+        } else {
+
+            playing.removeArrow(this);
+
+        }
+
+    }
+
+    
+    public void draw(Graphics g, int xOffset) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        int drawX = (int) Math.round(x - xOffset);
+        int drawY = (int) Math.round(y);
+        AffineTransform oldXForm = g2d.getTransform();
+
+        if (!shoot) {
+
+            theta = playing.getAngle();
+            if (weapon.xFlipped == 0) {
+
+                theta += Math.PI;
+
+            }
+
+            if (!playing.arrows.isEmpty()) {
+
+                shoot = true;
+
+            }
+
+        }
+
+        g2d.rotate(theta, drawX - 5, drawY - 5);
+
+        if (Playing.gunIndex == 1) {
+
+            g.drawImage(arrowSpecial, drawX - 5, drawY - 40, 42, 42, null);
+
+        } else if (Playing.gunIndex == 2) {
+
+            g.drawImage(arrowNormal, drawX - 5, drawY - 10, 42, 42, null);
+
+        }
+        g2d.setTransform(oldXForm);
+
+        if (playing.arrows.size() > 0) {
+
+            if (drawX >= weapon.x + 400 - xOffset || drawX >= GAME_WIDTH || drawX <= 0 || drawX <= weapon.x - xOffset - 400 || drawY <= 0 || drawY >= GAME_HEIGHT) {
+
+                playing.removeArrow(this);
+
+            }
+
+        }
+
+    }
+
+    
+    public void getImage() {
+
+        BufferedImage img = getSpriteAtlas(ARROW_ATLAS);
+        arrowNormal = img.getSubimage(0, 0, 42, 56);
+        arrowSpecial = img.getSubimage(0, 56, 42, 56);
+
+    }
+
+    
+    public void updateArrows() {
+
+        try {
+
+            for (Arrows arrow : playing.arrows) {
+
+                arrow.move();
+                this.time += 0.008;
+
+            }
+
+        } catch (ConcurrentModificationException e) {
+
+            // Handle exception
+
+        }
+
+    }
+
+   
+    public void handleCollision() {
+
+        for (Entity entity : playing.getEntities()) {
+
+            if (hitbox.intersects(entity.getHitbox())) {
+
+                entity.takeDamage(damage);
+                playing.removeArrow(this);
+                break;
+
+            }
+
+        }
+
+    }
+
+    
+    public void setTime() {
+
+        this.time = 0;
+
+    }
+
+    public double getTime() {
+
+        return this.time;
+
+    }
+
+    public int getDrawX() {
+
+        return (int) Math.round(x);
+
+    }
+
+    public int getDrawY() {
+
+        return (int) Math.round(y);
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
--- Src/Weapons/Bow.java
+++ Src/Weapons/Bow.java
@@ -1,6 +1,7 @@
 package Weapons;
 
 import Entities.*;
+import Entity.Player;
 import GameStates.Playing;
 import UserInputs.MouseInputs;
 import java.awt.Graphics;
@@ -15,5 +16,142 @@ import static Utilities.Constants.Directions.RIGHT;
 
 public class Bow implements MouseMotionListener {
 
-    
+    private Player player;
+    BufferedImage img;
+    Graphics2D g2d;
+    MouseInputs mouse;
+    Playing playing;
+
+    public int xFlipped = 0; 
+    private int wFlipped = 1; 
+    private float xOffset = 0; 
+    protected float x = 0;
+    protected float y = 0;
+
+    public Bow(Player player, Playing playing) {
+
+        this.player = player;
+        this.playing = playing;
+        getImage();
+
+    }
+
+    public double setAngle(double centerY, double mouseY, double centerX, double mouseX) {
+
+        return Math.atan2(centerY - mouseY, centerX - mouseX) - Math.PI / 2;
+
+    }
+
+    public void draw(Graphics g, int xOffset) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        AffineTransform oldXForm = g2d.getTransform();
+        g2d.rotate(playing.getAngle(), this.x + 30 - xOffset, this.y + 50);
+        this.xOffset = xOffset; 
+
+        if (!Playing.paused && !Playing.inventory && !Playing.dead && !Playing.endGame) {
+
+            if (Playing.bowIndex < 3) {
+
+                if (playing.mouseX < this.x - xOffset) {
+
+                    this.xFlipped = 0; 
+                    this.wFlipped = 1; 
+
+                } else {
+
+                    this.xFlipped = WEAPON_WIDTH;
+                    this.wFlipped = -1; 
+
+                }
+            } else {
+                if (playing.getPlayer().getDirection() == RIGHT) {
+
+                    g2d.setTransform(oldXForm);
+                    this.xFlipped = 46;
+                    this.wFlipped = -1; 
+
+                } else {
+
+                    g2d.setTransform(oldXForm);
+                    this.xFlipped = 0; 
+                    this.wFlipped = 1; 
+
+                }
+            }
+        }
+
+        g.drawImage(this.img, (int) this.x + this.xFlipped - xOffset, (int) this.y + 20, WEAPON_WIDTH * this.wFlipped, WEAPON_HEIGHT, null);
+        g2d.setTransform(oldXForm);
+
+    }
+
+    public void update() {
+
+        if (playing.mouseX < player.getHitbox().getCenterX() - xOffset)
+            this.x = (float) player.getHitbox().getCenterX() - player.getHitbox().width;
+
+        else
+
+            this.x = (float) player.getHitbox().getCenterX();
+        
+        this.y = player.getHitbox().y - 20;
+
+    }
+
+    public void getImage() {
+
+        if (Playing.bowIndex == 1) {
+
+            this.img = getSpriteAtlas(BOW1_ATLAS);
+
+        } else if (Playing.bowIndex == 2) {
+
+            this.img = getSpriteAtlas(BOW2_ATLAS);
+
+        } else if (Playing.bowIndex == 3) {
+
+            if (playing.getPlayer().getItemQuantity(2) > 0) {
+
+                this.img = getSpriteAtlas(BOMB_ATLAS);
+
+            } else {
+
+                this.img = getSpriteAtlas(NOTHING_ATLAS);
+
+            }
+            
+        }
+
+    }
+
+    public float getX() {
+        
+        return this.x;
+
+    }
+
+    public float getY() {
+
+        return this.y;
+
+    }
+
+    /* Mouse events */
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+        // add later if needed
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        
+        throw new UnsupportedOperationException("doesn't exist :)'");
+
+    }
 }
+
+
